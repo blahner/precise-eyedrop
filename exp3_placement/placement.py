26a6084a3c07830e7e5e1e4f6785d67de3746637
blahner <benlahner@gmail.com>
2024-03-11 16:37:59 -0400

    edit and clean exp3 files

--- exp3_placement/placement.py
+++ exp3_placement/placement.py
@@ -7,23 +7,26 @@ Created on Mon May  8 20:29:35 2023
 """
 
 import os
-import glob
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import pandas as pd
 import seaborn as sns
 import numpy as np
-import scipy
 import scipy.io as sio
 
-#plot the ball-in-tube feedback mechanism
+root = os.path.join("/home", "blahner", "projects", "precise-eyedrop") #your path to project root
+save_path = os.path.join(root, "exp3_placement", "output")
+data_path = os.path.join(root, "exp3_placement", "data")
 
-root = '/Users/blahner/Documents/Python/ocular275/parameterverification'
-is_save = False
-data_path = os.path.join(root, 'data')
-save_path = os.path.join(root, 'plots')
-data = pd.read_excel(os.path.join(data_path, "parameter_verificationV2.xlsx"))
-tmp = sio.loadmat("/Users/blahner/Documents/MATLAB/ECG275/angle25validf_d.mat")
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+is_save=True
+data = pd.read_excel(os.path.join(data_path, "parameter_verification.xlsx"))
+
+#this file contains predicted hit or miss for many pairs of (x, z) bottle tip placement.
+#used just to overlay patch on plot, not essential to results.
+tmp = sio.loadmat(os.path.join(data_path, "angle25validf_d.mat")) 
 tmp_results = tmp['results']
 bounds = {"x": [], "z": [], "hit": []}
 for r in range(tmp_results.shape[0]):
@@ -34,7 +37,6 @@ for r in range(tmp_results.shape[0]):
 df_theory = pd.DataFrame(bounds)
 idx = (df_theory["hit"] == True).values
 df_hits = df_theory.iloc[idx,:2]
-
 
 df_bottom = df_hits.iloc[(df_hits["z"] == np.min(df_hits["z"])).values,:]
 df_top= df_hits.iloc[(df_hits["z"] == np.max(df_hits["z"])).values,:]
@@ -48,21 +50,17 @@ lr = tuple(df_bottom.iloc[np.argmax(df_bottom["x"]) ,:])#lower right
 ul = tuple(df_top.iloc[np.argmin(df_top["x"]) ,:]) #upper left
 ur = tuple(df_top.iloc[np.argmax(df_top["x"]) ,:]) #upper right
 
-x_truth = 5
-y_truth = 0
-z_truth = 12
+#where we actually designed the device to place the bottle tip
+x_truth = 5 #in mm
+y_truth = 0 #in mm
+z_truth = 12 #in mm
 
 ax = sns.scatterplot(data=data, x="x",y="z")
 plt.plot(x_truth, z_truth, color = 'red', marker='o')
-#sns.scatterplot(data=df_hits, x="x", y="z") #add all hit points
 ax.add_patch(patches.Polygon([la, ll, lr, ur, ul], fill=True, alpha=0.2, color='red'))
-#plt.xlim([0,10])
-#plt.ylim([0,22])
 
 if is_save:
     plt.savefig(os.path.join(save_path, "parameterverification_scatterplot.svg"))
     plt.savefig(os.path.join(save_path, "parameterverification_scatterplot.png"))
-
 plt.show()
-
-#perform stats
+plt.clf()
